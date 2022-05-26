1c1aebd159ba75a0229a22f4f57e4d9dee36e4be
shige <example@email.com>
2022-05-26 04:15:40 +0000

    laravel_12kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,16 @@
-@extends('layouts.admin')
 
-@section('title', 'ニュースの新規作成')
+@extends('layouts.profile')
+
+
+
+@section('title', 'Myプロフィール')
 
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
+                <h2>Myプロフィール</h2>
             </div>
         </div>
     </div>
