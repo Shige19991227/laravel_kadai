d67326b142305ae3a64b9562eaf7273d64dcc5a2
shige <example@email.com>
2022-05-30 07:03:08 +0000

    Q

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -15,7 +15,7 @@ class ProfileController extends Controller
     }
     public function create(Request $request)
   {
-    $this->validate($request, profile::$rules);
+    $this->validate($request, Profile::$rules);
     
      $profile = new Profile;
       $form = $request->all();
@@ -31,9 +31,26 @@ class ProfileController extends Controller
         return view('admin.profile.edit');
     }
 
-    public function update()
-    {
+ public function update(Request $request)
+  {
+     // Validationをかける
+     $this->validate($request, Profile::$rules);
+     // Profile Modelからデータを取得する
+     $profile = Profile::find($request->id);
+
+     // 送信されてきたフォームデータを格納する
+     $profile_form = $request->all();
+
+     unset($profile_form['remove']);
+     //トークンの削除
+     unset($profile_form['_token']);
+     // 該当するデータを上書きして保存する
+     $profile->fill($profile_form)->save();
+
+        $profileHistory = new ProfileHistory;
+        $profileHistory->profile_id = $profile->id;
+        $profileHistory->edited_at = Carbon::now();
+        $profileHistory->save();
         return redirect('admin/profile/edit');
     }
-
 }
